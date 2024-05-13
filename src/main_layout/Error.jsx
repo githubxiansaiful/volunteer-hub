bd50c054664b06021c0948b272f7d01d922ab91f
Xian Saiful <xiansaiful@gmail.com>
2024-05-14 01:52:50 +0600

    Changed background color on 404 page

--- src/main_layout/Error.jsx
+++ src/main_layout/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
             <Helmet>
                 <title>404 Not Found | Volunteer Hub</title>
             </Helmet>
-            <main className="grid h-[80vh] place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <main className="grid h-[80vh] place-items-center px-6 py-24 sm:py-32 lg:px-8">
                 <div className="text-center">
                     <p className="text-base font-semibold text-indigo-600">404</p>
                     <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
